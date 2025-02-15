09405054e3778a678e27a87aa2b92f9fbc989ac4
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2025-02-15 14:50:02 +0100

    Scrollbar

--- app/components/ArticleCard/ArticleCard.tsx
+++ app/components/ArticleCard/ArticleCard.tsx
@@ -1,12 +1,12 @@
-"use client" // Needed for next.js when using useState
+'use client' // Needed for next.js when using useState
 
 import styles from "./ArticleCard.module.css";
-import { redirect } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 export default function ArticleCard({ post }: { post: Post }) {
-
+    const router = useRouter()
     return (
-        <div className={styles.container} onClick={() => redirect(`/posts/${post.id}`)}>
+        <div className={styles.container} onClick={() => router.push(`/posts/${post.id}`)}>
             <div className={styles.articlePreview}>
                 <h1 className="text-xl">{post.title}</h1>
                 <p className="text-sm opacity-70 italic">{post.createdAt.toString()}</p>
