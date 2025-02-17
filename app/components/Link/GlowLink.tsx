15cd6cfd6af3766785d7e54d399945530633aa93
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2025-02-17 16:32:52 +0100

    Coloured GlowUI links

--- app/components/Link/GlowLink.tsx
+++ app/components/Link/GlowLink.tsx
@@ -3,13 +3,20 @@
 import styles from "./GlowLink.module.css";
 
 interface props { // Specifies types for prop parameters
-    href: string;
-    children: React.ReactNode;
+    href?: string;
+    color?: string
+    children?: React.ReactNode;
 }
 
-export default function GlowLink({href, children}: props) {
+export default function GlowLink({href, color, children}: props) {
+
+    /*
+     * Use color prop if specified, otherwise use accent color as default
+     * We then pass it in to the CSS module as the --glow-color variable
+     */
+    const GlowColor = color || "var(--accent)" 
     return(
-        <div className={styles.container}>
+        <div className={styles.container} style={{ "--glow-color": GlowColor } as React.CSSProperties}>
             <a href={href} target="_blank" className={styles.link}>{children}</a>
             <div className={styles.glowbar}/>
         </div>
